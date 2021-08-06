537f18256c1ad4812726ed660fcbfa518d1ef5d9
poyaowang <michael90110@gmail.com>
2021-08-06 17:30:39 +0900

    030 The "never" Type

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ userInput = "Max";
 if (typeof userInput === "string") {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
+}
+generateError("An error occurred!", 500);
