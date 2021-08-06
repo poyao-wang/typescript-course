a5a67324ae5da1dff15b9fe865e142eccc699bd1
poyaowang <michael90110@gmail.com>
2021-08-06 16:29:43 +0900

    027 Functions as Types

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ function printResult(num) {
     console.log("Result: " + num);
 }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
