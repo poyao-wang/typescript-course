6727ae33c9b20576edcc7bee8f3f5268261379b2
poyaowang <michael90110@gmail.com>
2021-08-06 15:36:55 +0900

    024 Type Aliases / Custom Types

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
