3aeb4fea6aefbabe5a723cce0c2ce4055a59221f
poyaowang <michael90110@gmail.com>
2021-08-08 18:28:50 +0900

    044 Code Quality Options

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,17 @@
 "use strict";
-const userName = 'Maximilian';
-console.log(userName);
+let appId = 'abc';
+const button = document.querySelector('button');
+function add(n1, n2) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+}
+function clickHandler(message) {
+    // let userName = 'Max';
+    console.log('Clicked! ' + message);
+}
+// a comment
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+}
