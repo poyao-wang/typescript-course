beb549bbac585e85e4611c9c02e93a847c7698f6
poyaowang <michael90110@gmail.com>
2021-08-10 16:31:27 +0900

    052 The Spread Operator

--- dist/app.js
+++ dist/app.js
@@ -13,10 +13,20 @@ age = 29;
 // }
 // console.log(isOld);
 // console.log(result);
-const add = (a, b = 1) => a + b;
-const printOutput = output => console.log(output);
-const button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', event => console.log(event));
-}
-printOutput(add(5));
+// const add = (a: number, b: number = 1) => a + b;
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
+// const button = document.querySelector("button");
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
+// printOutput(add(5));
+// const hobbies = ["Sports", "Cooking"];
+// const activeHobbies = ["Hiking", ...hobbies];
+// console.log(activeHobbies);
+const person = {
+    name: "Max",
+    age: 30,
+};
+const copiedPerson = Object.assign({}, person);
+console.log(copiedPerson);
--- src/app.ts
+++ src/app.ts
@@ -18,14 +18,29 @@ age = 29;
 
 // console.log(result);
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector("button");
 
-if (button) {
-  button.addEventListener('click', event => console.log(event));
-}
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
+
+// const hobbies = ["Sports", "Cooking"];
+// const activeHobbies = ["Hiking", ...hobbies];
+
+// console.log(activeHobbies);
+
+const person = {
+  name: "Max",
+  age: 30,
+};
+
+const copiedPerson = { ...person };
+
+console.log(copiedPerson);
