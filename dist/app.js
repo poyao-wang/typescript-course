8c323fe38c869ddd5491d075f57c75f19a3a08db
poyaowang <michael90110@gmail.com>
2021-08-10 17:10:41 +0900

    054 Array & Object Destructuring

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,8 @@
 "use strict";
-const userName = "Max";
-// userName = 'Maximilian';
-let age = 30;
-age = 29;
+// const userName = 'Max';
+// // userName = 'Maximilian';
+// let age = 30;
+// age = 29;
 // function add(a: number, b: number) {
 //   let result;
 //   result = a + b;
@@ -14,25 +14,28 @@ age = 29;
 // console.log(isOld);
 // console.log(result);
 // const add = (a: number, b: number = 1) => a + b;
-// const printOutput: (a: number | string) => void = (output) =>
-//   console.log(output);
-// const button = document.querySelector("button");
+// const printOutput: (a: number | string) => void = output => console.log(output);
+// const button = document.querySelector('button');
 // if (button) {
-//   button.addEventListener("click", (event) => console.log(event));
+//   button.addEventListener('click', event => console.log(event));
 // }
 // printOutput(add(5));
-// const hobbies = ["Sports", "Cooking"];
-// const activeHobbies = ["Hiking", ...hobbies];
-// console.log(activeHobbies);
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+activeHobbies.push(...hobbies);
 const person = {
-    name: "Max",
+    firstName: "Max",
     age: 30,
 };
-const copiedPerson = Object.assign({}, person);
-const add = (...numbers) => {
-    return numbers.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
-};
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
+// const copiedPerson = { ...person };
+// const add = (...numbers: number[]) => {
+//   return numbers.reduce((curResult, curValue) => {
+//     return curResult + curValue;
+//   }, 0);
+// };
+// const addedNumbers = add(5, 10, 2, 3.7);
+// console.log(addedNumbers);
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-const userName = "Max";
-// userName = 'Maximilian';
-let age = 30;
+// const userName = 'Max';
+// // userName = 'Maximilian';
+// let age = 30;
 
-age = 29;
+// age = 29;
 
 // function add(a: number, b: number) {
 //   let result;
@@ -20,34 +20,41 @@ age = 29;
 
 // const add = (a: number, b: number = 1) => a + b;
 
-// const printOutput: (a: number | string) => void = (output) =>
-//   console.log(output);
+// const printOutput: (a: number | string) => void = output => console.log(output);
 
-// const button = document.querySelector("button");
+// const button = document.querySelector('button');
 
 // if (button) {
-//   button.addEventListener("click", (event) => console.log(event));
+//   button.addEventListener('click', event => console.log(event));
 // }
 
 // printOutput(add(5));
 
-// const hobbies = ["Sports", "Cooking"];
-// const activeHobbies = ["Hiking", ...hobbies];
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
 
-// console.log(activeHobbies);
+activeHobbies.push(...hobbies);
 
 const person = {
-  name: "Max",
+  firstName: "Max",
   age: 30,
 };
 
-const copiedPerson = { ...person };
+// const copiedPerson = { ...person };
 
-const add = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
+// const add = (...numbers: number[]) => {
+//   return numbers.reduce((curResult, curValue) => {
+//     return curResult + curValue;
+//   }, 0);
+// };
 
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
+// const addedNumbers = add(5, 10, 2, 3.7);
+// console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
