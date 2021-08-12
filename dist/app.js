644ce5924bc6985fa69126c6c4642d3644a2288d
poyaowang <michael90110@gmail.com>
2021-08-12 21:45:54 +0900

    070 Singletons & Private Constructors

--- dist/app.js
+++ dist/app.js
@@ -49,6 +49,13 @@ class AccountingDepartment extends Department {
         }
         this.addReport(value);
     }
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2', []);
+        return this.instance;
+    }
     describe() {
         console.log('Accounting Department - ID: ' + this.id);
     }
@@ -76,7 +83,10 @@ it.describe();
 it.name = 'NEW NAME';
 it.printEmployeeInformation();
 console.log(it);
-const accounting = new AccountingDepartment('d2', []);
+// const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+console.log(accounting, accounting2);
 accounting.mostRecentReport = 'Year End Report';
 accounting.addReport('Something went wrong...');
 console.log(accounting.mostRecentReport);
