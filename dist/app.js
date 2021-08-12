2b5a044d19ec8f2eabaa649c3509aa9f04e378b8
poyaowang <michael90110@gmail.com>
2021-08-12 16:14:23 +0900

    067 Getters & Setters

--- dist/app.js
+++ dist/app.js
@@ -30,6 +30,19 @@ class AccountingDepartment extends Department {
     constructor(id, reports) {
         super(id, "Accounting");
         this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.');
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
     }
     addEmployee(name) {
         if (name === "Max") {
@@ -39,6 +52,7 @@ class AccountingDepartment extends Department {
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReports() {
         console.log(this.reports);
@@ -53,7 +67,9 @@ it.name = 'NEW NAME';
 it.printEmployeeInformation();
 console.log(it);
 const accounting = new AccountingDepartment('d2', []);
+accounting.mostRecentReport = 'Year End Report';
 accounting.addReport('Something went wrong...');
+console.log(accounting.mostRecentReport);
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
 accounting.printReports();
