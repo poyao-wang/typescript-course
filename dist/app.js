f43b818f183908d6b4e65c5ec078a6987169e7b3
poyaowang <michael90110@gmail.com>
2021-08-13 10:13:30 +0900

    073 Using Interfaces with Classes

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,14 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Max',
-    age: 30,
+class Person {
+    constructor(n) {
+        this.age = 30;
+        this.name = n;
+    }
     greet(phrase) {
         console.log(phrase + ' ' + this.name);
     }
-};
+}
+let user1;
+user1 = new Person('Max');
 user1.greet('Hi there - I am');
+console.log(user1);
