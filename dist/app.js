c44c9347dfa04f509e58c896e8794594d607fac8
poyaowang <michael90110@gmail.com>
2021-08-13 20:13:35 +0900

    078 Optional Parameters & Properties

--- dist/app.js
+++ dist/app.js
@@ -6,14 +6,21 @@ add = (n1, n2) => {
 class Person {
     constructor(n) {
         this.age = 30;
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
     }
     greet(phrase) {
-        console.log(phrase + ' ' + this.name);
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        }
+        else {
+            console.log('Hi!');
+        }
     }
 }
 let user1;
-user1 = new Person('Max');
+user1 = new Person();
 // user1.name = 'Manu';
 user1.greet('Hi there - I am');
 console.log(user1);
