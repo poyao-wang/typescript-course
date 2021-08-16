e3a8d02406a05e35a9fc804e9900955017758680
poyaowang <michael90110@gmail.com>
2021-08-16 14:13:34 +0900

    088 Function Overloads

--- dist/app.js
+++ dist/app.js
@@ -10,55 +10,6 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployeeInformation(emp) {
-    console.log('Name: ' + emp.name);
-    if ('privileges' in emp) {
-        console.log('Privileges: ' + emp.privileges);
-    }
-    if ('startDate' in emp) {
-        console.log('Start Date: ' + emp.startDate);
-    }
-}
-printEmployeeInformation({ name: 'Manu', startDate: new Date() });
-class Car {
-    drive() {
-        console.log('Driving...');
-    }
-}
-class Truck {
-    drive() {
-        console.log('Driving a truck...');
-    }
-    loadCargo(amount) {
-        console.log('Loading cargo ...' + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-    }
-    console.log('Moving at speed: ' + speed);
-}
-moveAnimal({ type: 'bird', flyingSpeed: 10 });
-const userInputElement = document.getElementById('user-input');
-userInputElement.value = 'Hi there!';
-const errorBag = {
-    email: 'Not a valid email!',
-    username: 'Must start with a capital character!',
-};
+const result = add('Max', ' Schwarz');
+result.split(' ');
 //# sourceMappingURL=app.js.map
