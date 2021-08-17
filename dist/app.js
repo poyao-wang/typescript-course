1e84bbddec714c620902a691a7426a6cb0b3113b
poyaowang <michael90110@gmail.com>
2021-08-17 11:26:45 +0900

    096  Working with Constraints

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,9 @@
 "use strict";
-const names = [];
-names[0].split(' ');
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const objA = { name: 'Max', hobbies: ['Sports'] };
+const objB = { age: 30 };
+const mergedObj = merge(objA, objB);
+console.log(mergedObj);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,21 @@
-const names: Array<string> = [];
-names[0].split(' ');
+// const names: Array<string> = [];
+// names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Resolved');
-  }, 1000);
-});
+// const promise: Promise<string> = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve('Resolved');
+//   }, 1000);
+// });
 
-promise.then((data) => {
-  data.split(' ');
-});
+// promise.then((data) => {
+//   data.split(' ');
+// });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 const objA = { name: 'Max', hobbies: ['Sports'] };
 const objB = { age: 30 };
 const mergedObj = merge(objA, objB);
+console.log(mergedObj);
