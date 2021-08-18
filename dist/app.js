f39f5eb23cc31646fc0c5dfa32d4932128f72e3e
poyaowang <michael90110@gmail.com>
2021-08-18 17:16:56 +0900

    102  Generic Types vs Union Types

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,24 @@
 "use strict";
-function createCourseGoal(title, description, date) {
-    let courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal;
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
 }
-const names = ['Max', 'Anna'];
+const textStorage = new DataStorage();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -42,30 +42,30 @@
 
 // extractAndConvert({ name: 'Max' }, 'name');
 
-// class DataStorage<T extends string | number | boolean> {
-//   private data: T[] = [];
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
 
-//   addItem(item: T) {
-//     this.data.push(item);
-//   }
+  addItem(item: T) {
+    this.data.push(item);
+  }
 
-//   removeItem(item: T) {
-//     if (this.data.indexOf(item) === -1) {
-//       return;
-//     }
-//     this.data.splice(this.data.indexOf(item), 1); // -1
-//   }
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1); // -1
+  }
 
-//   getItems() {
-//     return [...this.data];
-//   }
-// }
+  getItems() {
+    return [...this.data];
+  }
+}
 
-// const textStorage = new DataStorage<string>();
-// textStorage.addItem('Max');
-// textStorage.addItem('Manu');
-// textStorage.removeItem('Max');
-// console.log(textStorage.getItems());
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
 
 // const numberStorage = new DataStorage<number>();
 
@@ -77,24 +77,24 @@
 // objStorage.removeItem(maxObj);
 // console.log(objStorage.getItems());
 
-interface CourseGoal {
-  title: string;
-  description: string;
-  completeUntil: Date;
-}
+// interface CourseGoal {
+//   title: string;
+//   description: string;
+//   completeUntil: Date;
+// }
 
-function createCourseGoal(
-  title: string,
-  description: string,
-  date: Date
-): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
-}
+// function createCourseGoal(
+//   title: string,
+//   description: string,
+//   date: Date
+// ): CourseGoal {
+//   let courseGoal: Partial<CourseGoal> = {};
+//   courseGoal.title = title;
+//   courseGoal.description = description;
+//   courseGoal.completeUntil = date;
+//   return courseGoal as CourseGoal;
+// }
 
-const names: Readonly<string[]> = ['Max', 'Anna'];
+// const names: Readonly<string[]> = ['Max', 'Anna'];
 // names.push('Manu');
 // names.pop();
