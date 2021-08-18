ff4128d5c028efad52b5af5a61f928cf380f5307
poyaowang <michael90110@gmail.com>
2021-08-18 21:50:28 +0900

    clear before section 8
    "experimentalDecorators": true

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,2 @@
 "use strict";
-class DataStorage {
-    constructor() {
-        this.data = [];
-    }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
-        }
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.data];
-    }
-}
-const textStorage = new DataStorage();
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-textStorage.removeItem('Max');
-console.log(textStorage.getItems());
 //# sourceMappingURL=app.js.map
