08e303b8e442f7e8956aa37b789f33cae1414874
poyaowang <michael90110@gmail.com>
2021-08-26 17:17:36 +0900

    146  Using ES Modules

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,9 +1,8 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../util/validation.ts" />
-/// <reference path="../state/project-state.ts" />
+import { Component } from './base-component.js';
+import { Validatable, validate } from '../util/validation.js';
+import { autobind } from '../decorators/autobind.js';
+import { projectState } from '../state/project-state.js';
 
-namespace App {
   // ProjectInput Class
   export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     titleInputElement: HTMLInputElement;
@@ -80,5 +79,3 @@ namespace App {
       }
     }
   }
-
-}
