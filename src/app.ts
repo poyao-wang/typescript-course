a7be842acdb8314d8bba2a46b5fc7cac10b024a6
poyaowang <michael90110@gmail.com>
2021-09-07 21:16:51 +0900

    169 Getting User Input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const from = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // send this to Google api
+}
+
+from.addEventListener('submit', searchAddressHandler);
